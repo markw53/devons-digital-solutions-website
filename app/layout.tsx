5965f4331ea2ce1171024ffd22f7bc5e0e9e1fe9
markw53 <m.workman@outlook.com>
2025-03-15 21:44:49 +0000

    updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import PageTransition from "./components/PageTransition";
 import "./globals.css";
 
 const geistSans = Geist({
